607faba610a1a3f1af5841b173d688d8f5353cdc
Shaun Meechan <shaunmeechan@hotmail.co.uk>
2020-06-17 14:18:08 +0100

    Store is now working with skins
    Player has to re-buy skins to change back. This will be fixed soon (hopefully the next commit)

--- How to Tennis/Assets/Scripts/ShopSystem.cs
+++ How to Tennis/Assets/Scripts/ShopSystem.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShopSystem : MonoBehaviour
 {
     public Player player;
+    public TextMeshProUGUI creditsText;
     public void buyItem(SkinObject item, int cost)
     {
+        Debug.Log("Player has attempted to buy an item!");
         if (player.credits >= cost)
         {
             //Player can afford the item. Allow purchase
             player.skin = item;
             player.credits -= cost;
+            Debug.Log("Player purcahsed an item!");
+            //Update the credits text
+            creditsText.text = "Credits: " + player.credits;
             //Play a noise
         }
         else
         {
             //Player can't afford the item. Don't allow purchase
             //Play a noise and show an icon or something
+            Debug.Log("Player can't afford item");
         }
     }
 }
---
+++ How to Tennis/Assets/Scripts/StoreButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreButton : MonoBehaviour
+{
+    public SkinObject linkedSkin;
+    public ShopSystem shopSystem;
+    public void run()
+    {
+        //Call the shop system purchase function
+        shopSystem.buyItem(linkedSkin, linkedSkin.cost);
+    }
+}
