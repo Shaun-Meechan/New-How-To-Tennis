314d114f3061accdeaf8dd7e0caf89472f093a06
Shaun Meechan <shaunmeechan@hotmail.co.uk>
2020-06-22 16:01:55 +0100

    Added loading screens

---
+++ How to Tennis/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public Slider slider;
+    public TextMeshProUGUI percentageText;
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            percentageText.text = (progress * 100f).ToString() + "%";
+            yield return null;
+        }
+    }
+}
